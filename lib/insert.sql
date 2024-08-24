dcf4a8fc92ef9aa618b87e3b1ea7fe7fc9f484a1
Samantha <samanthavillafana8@gmail.com>
2024-08-24 14:31:55 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 4, 'M', 'black', 'aggressive', 1),
+('Tabitha', 6, 'F', 'brown', 'calm', 1),
+('Sergeant Brown', 9, 'M', 'brown', 'aggressive', 1),
+('Melissa', 5, 'F', 'black', 'calm', 1),
+('Grinch', 3, 'M', 'black', 'grumpy', 1),
+('Wendy', 7, 'F', 'brown', 'friendly', 1),
+(NULL, 8, 'M', 'brown', 'aggressive', 1);  -- unnamed bear
